3d9abb9b59ce61181b815521763d80d3add7ce36
Shahrukh Ali <srk1581998@gmail.com>
2026-02-26 00:08:17 +0530

    Node class structure add

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Node {
+public class Node {
+    int data;
+    Node next;
+
+
+    public Node(int data) {
+        this.data=data;
+        this.next=null;
+    }
 }
