6e7380d4f6e703cf13b0b65dec69335c9be2d4a7
Shahrukh Ali <srk1581998@gmail.com>
2026-02-26 14:48:35 +0530

    implementation of node AtFront

--- LinkedList/InsertAtFront.java
+++ LinkedList/InsertAtFront.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class InsertAtFront {
+public class InsertAtFront {
+    static Node insertAtFront(Node head, int x) {
+        Node newNode = new Node(x);
+        newNode.next = head;
+        return newNode;
+    }
+
+    // Function to print the contents
+    // of the linked list
+    static void printList(Node head) {
+        Node curr = head;
+
+        while (curr != null) {
+            System.out.print(curr.data);
+            if (curr.next != null) {
+                System.out.print(" -> ");
+            }
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create the linked list 2->3->4->5
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+
+        // Insert a new node at the
+        // front of the list
+        int x = 1;
+        head = insertAtFront(head, x);
+
+        printList(head);
+    }
 }
